50925e2307fe388eebee877bdb6d7b0951e54db8
Emir Huaroc <emirmho3@gmail.com>
2023-09-27 16:44:09 -0500

    funcionalidad mostrar productos

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -1,11 +1,13 @@
 package com.curso.ecommerce.controller;
 
+
 import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.model.Usuario;
 import com.curso.ecommerce.service.ProductoService;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +26,8 @@ public class ProductoController {
     }
 
     @GetMapping("/create")
-    public String create(){
+    public String create(Model model){
+        model.addAttribute("productos",productoService.findAll());
         return "administrador/productos/create";
     }
 
